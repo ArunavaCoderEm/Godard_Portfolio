3f3f73ab8ac9eeaf130d428ac998b79fa23bbd3d
ArunavaCoderEm <acecankill@gmail.com>
2024-07-03 00:56:40 +0530

    Pushes done

--- src/Page.tsx
+++ src/Page.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Navbar from './Components/Navbar'
+import BackgroundGrid from './Components/Bgmark'
 
 export default function Page():React.ReactNode {
   return (
     <>
         <Navbar />
+        <BackgroundGrid />
         <Routes>
             <Route path='/' element={<Home />} />
         </Routes>
