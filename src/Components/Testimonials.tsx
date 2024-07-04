2fd2c814e56e8747c41870a3bc84d82233067a57
ArunavaCoderEm <acecankill@gmail.com>
2024-07-04 22:27:25 +0530

    push

--- src/Components/Testimonials.tsx
+++ src/Components/Testimonials.tsx
@@ -1,104 +1,94 @@
-import React from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 interface testlist {
-    text: string;
-    author: string;
-    image: string;
-    link: string;
+  text : string,
+  name : string,
+  position : string,
+  imgSrc : string,
+  link : string
 }
 
 const testimonials : testlist[] = [
   {
-    text: 'Very Helpful ! Sufficiently satisfied ! Creativity and uniqueness is the best part.',
-    author: 'Sohan Karfa',
-    image: './sohan.jpg',
-    link : "https://www.linkedin.com/in/sohan-karfa/"
+    text: "Very supportive teammate! Impressed by innovative and original approach.",
+    name: 'Satyaki Dey',
+    position: 'Product Manager, Intel',
+    imgSrc: './satyaki.jpg',
+    link: "https://www.linkedin.com/in/satyaki-dey/"
   },
   {
-    text: "Very supportive friend and teammate! Impressed by the innovative and original approach.",
-    author: 'Satyaki Dey',
-    image: './satyaki.jpg',
-    link : "https://www.linkedin.com/in/satyaki-dey/"
+    text: "Very Helpful ! Sufficiently satisfied ! Uniqueness is the best part.",
+    name: 'Sohan Karfa',
+    position: 'UX Designer, Netflix',
+    imgSrc: './sohan.jpg',
+    link: "https://www.linkedin.com/in/sohan-karfa/"
   },
   {
-    text: 'Incredibly supportive! Completely satisfied! Loved the innovative approach.',
-    author: 'Priyanshu Dutta',
-    image: './priyan.jpg',
-    link : "https://www.linkedin.com/in/priyanshudutta04/   "
+    text: "Incredibly supportive! Loved the innovative approach.",
+    name: 'Priyanshu Dutta',
+    position: 'Data Analyst, Spotify',
+    imgSrc: './priyan.jpg',
+    link: "https://www.linkedin.com/in/priyanshudutta04/"
   },
   {
-    text: 'Quite beneficial! Quite satisfied with the creative and original approach',
-    author: 'Rishi Bhattasali',
-    image: './rishi.jpg',
-    link : "https://www.linkedin.com/in/rishi-bhattasali-8ab905253/"
+    text: "Quite beneficial! Quite satisfied with the creative and original approach",
+    name: 'Rishi Bhattasali',
+    position: 'Data Analyst, Spotify',
+    imgSrc: './rishi.jpg',
+    link: "https://www.linkedin.com/in/rishi-bhattasali-8ab905253/"
   },
 ];
 
-const TestimonialCarousel = ():React.ReactNode => {
+const TestimonialSlider = ():React.ReactNode => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const [currentTestimonial, setCurrentTestimonial] = useState(0);
+  const scrollTo = (index:number) => {
+    setCurrentIndex(index);
+  };
 
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setCurrentTestimonial((prevTestimonial) => (prevTestimonial + 1) % testimonials.length);
-      }, 5000);
-  
-      return () => {
-        clearInterval(intervalId);
-      };
-    }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+    }, 5000);
 
-    const variants = {
-        initial: { opacity: 0 },
-        animate: { opacity: 1 },
-        exit: { opacity: 0 },
-      };
-  
-    const { text, author, image, link } = testimonials[currentTestimonial];
-
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <>
-    <div className="bg-gradient-to-bl sha from-[#D29EFB] to-[#6e0eb7] absolute right-0 left-0 mx-auto w-5/6 h-60 p-2 flex rounded-lg mt-10 flex-col items-center justify-center">
-      <section className="py-2 md:py-3 w-full">
-      <h2 className='text-center text-xl font-extrabold backdrop-blur-sm bg-white/30 inssha p-1 rounded-full mukta mb-3 w-72 mx-auto'>🔈 Testimonials 🔈</h2>
-        <div className="w-full max-w-2xl flex flex-col mx-auto">
-          <AnimatePresence mode="popLayout">
-            <motion.div
-              key={currentTestimonial}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              variants={variants}
-              className="flex w-full mt-[-20px] flex-col items-center justify-center"
-              transition={{
-                  type: 'spring',
-                  stiffness: 200,
-                  damping: 20,
-                  duration: 0.5,
-                }}
-                >
-              <img src={image} alt={author} className="h-10 w-10 m-5 sha rounded-full" />
-              <p className="mt-[-10px] text-center text-[#FFF250] text-lg font-medium tracking-tight">
-                &quot;{text}&quot;
-              </p>
-              <div className="mx-auto">
-                <div className="flex flex-col items-center justify-center space-x-3">
-                  <Link to={link} target='_blank' className="font-regular text-sm text-gray-900">
-                    - {author}
-                  </Link>
-                </div>
+    <main className="flex flex-col items-center  absolute w-5/6 mx-auto rounded-lg h-64 mt-10 bg-gradient-to-bl sha from-[#D29EFB] to-[#6e0eb7] p-1 left-0 right-0 gap-3 overflow-hidden">
+      <ul className="relative w-full h-48 rounded-lg p-2 ">
+        {testimonials.map((testimonial, index) => (
+          <li
+            key={index}
+            className={`absolute inset-0 flex items-center justify-center transition-opacity duration-1000 ${currentIndex === index ? 'opacity-100' : 'opacity-0'}`}
+          >
+            <Link to={testimonial.link} target='_blank' className="relative flex items-center">
+              <div className="w-3/4 p-8 pr-20 h-48 text-white rounded-xl opacity-0 transform translate-x-32 animate-slide-in">
+                <p className="font-bold text-md">{testimonial.text}</p>
+                <p className="italic text-sm mt-4 text-white/70">{testimonial.name}</p>
+                <p className="text-white/70 text-xs">{testimonial.position}</p>
               </div>
-            </motion.div>
-          </AnimatePresence>
-        </div>
-      </section>
-    </div>
-    </>
+              <img
+                className="absolute sha right-5 z-10 w-28 h-28 rounded-full object-cover transform -translate-x-0 animate-image-in"
+                src={testimonial.imgSrc}
+                alt={`${testimonial.name}`}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <nav>
+        {testimonials.map((_, index) => (
+          <button
+            key={index}
+            className={`w-3 h-3 rounded-full bg-[#FFF250] mx-1 transition-all ${currentIndex === index ? 'w-7' : ''}`}
+            onClick={() => scrollTo(index)}
+          ></button>
+        ))}
+      </nav>
+    </main>
   );
 };
 
-export default TestimonialCarousel;
+export default TestimonialSlider;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-in': {
+          '0%': { opacity: 0, transform: 'translateX(125px)' },
+          '50%': { opacity: 1 },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+        'image-in': {
+          '0%': { transform: 'translateX(-25px)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        content: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'slide-in': 'slide-in 1s ease-out forwards',
+        'image-in': 'image-in 1.25s ease-out forwards',
+        content: 'content 1s forwards',
+      },
+    },
   },
   plugins: [],
 }
