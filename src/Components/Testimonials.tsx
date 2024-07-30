a78d8056d89d476f5562002bfac85e13746c6fa5
ArunavaCoderEm <acecankill@gmail.com>
2024-07-30 20:49:53 +0530

    testimonials

--- src/Components/Testimonials.tsx
+++ src/Components/Testimonials.tsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-interface testlist {
-  text : string,
-  name : string,
-  position : string,
-  imgSrc : string,
-  link : string
+interface Testlist {
+  text: string;
+  name: string;
+  position: string;
+  imgSrc: string;
+  link: string;
 }
 
-const testimonials : testlist[] = [
+const testimonials: Testlist[] = [
   {
     text: "Very supportive teammate! Impressed by innovative and original approach.",
     name: 'Satyaki Dey',
-    position: 'Devops, App, Web Developer',
+    position: 'DevOps, App, Web Developer',
     imgSrc: './satyaki.jpg',
     link: "https://www.linkedin.com/in/satyaki-dey/"
   },
   {
-    text: "Very Helpful ! Sufficiently satisfied ! Uniqueness is the best part.",
+    text: "Very Helpful! Sufficiently satisfied! Uniqueness is the best part.",
     name: 'Sohan Karfa',
-    position: 'App developer, Coder',
+    position: 'App Developer, Coder',
     imgSrc: './sohan.jpg',
     link: "https://www.linkedin.com/in/sohan-karfa/"
   },
@@ -32,7 +32,7 @@ const testimonials : testlist[] = [
     link: "https://www.linkedin.com/in/priyanshudutta04/"
   },
   {
-    text: "Quite beneficial! Quite satisfied with the creative and original approach",
+    text: "Quite beneficial! Quite satisfied with the creative and original approach.",
     name: 'Rishi Bhattasali',
     position: 'ML Engineer',
     imgSrc: './rishi.jpg',
@@ -40,10 +40,10 @@ const testimonials : testlist[] = [
   },
 ];
 
-const TestimonialSlider = ():React.ReactNode => {
+const TestimonialSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const scrollTo = (index:number) => {
+  const scrollTo = (index: number) => {
     setCurrentIndex(index);
   };
 
@@ -56,17 +56,17 @@ const TestimonialSlider = ():React.ReactNode => {
   }, []);
 
   return (
-    <main className="flex flex-col items-center  absolute w-5/6 mx-auto rounded-lg h-64 mt-10 bg-gradient-to-bl sha from-[#D29EFB] to-[#6e0eb7] p-1 left-0 right-0 gap-3 overflow-hidden">
+    <main className="flex flex-col items-center absolute w-5/6 mx-auto rounded-lg h-64 mt-10 bg-gradient-to-bl from-[#D29EFB] to-[#6e0eb7] p-1 left-0 right-0 gap-3 overflow-hidden">
       <div className='text-center text-xl font-extrabold backdrop-blur-sm bg-white/30 inssha p-1 rounded-full mukta my-3'>🔈Testimonials 🔈</div>
-      <ul className="relative w-full h-48 rounded-lg p-2 ">
+      <ul className="relative w-full h-48 rounded-lg p-2">
         {testimonials.map((testimonial, index) => (
           <li
             key={index}
             className={`absolute inset-0 flex items-center justify-center transition-opacity duration-1000 ${currentIndex === index ? 'opacity-100' : 'opacity-0'}`}
           >
-            <Link to={testimonial.link} target='_blank' className="relative flex items-center">
+            <Link to={testimonials[currentIndex].link} target='_blank' className="relative flex items-center">
               <div className="w-3/4 p-8 pr-20 h-48 text-white rounded-xl opacity-0 transform translate-x-32 animate-slide-in">
-                <p className="font-bold text-md">{testimonial.text}</p>
+                <p className="font-bold text-xs lg:text-md lg:text-lg">{testimonial.text}</p>
                 <p className="italic text-sm mt-4 text-white/70">{testimonial.name}</p>
                 <p className="text-white/70 text-xs">{testimonial.position}</p>
               </div>
