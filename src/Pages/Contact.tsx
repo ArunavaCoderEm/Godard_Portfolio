1f0206435e940338ba5060627b083ae6693bc919
ArunavaCoderEm <acecankill@gmail.com>
2024-07-30 22:23:09 +0530

    contact push

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -11,56 +11,56 @@ const ContactForm = ():React.ReactNode => {
       <div className="flex flex-col p-2 md:flex-row justify-center items-start w-full max-w-4xl bg-gradient-to-bl from-[#FFF250] to-[#a09606] sha rounded-lg overflow-hidden">
 
         <div className="w-full md:w-1/3 lg:h-64 rounded-lg sha bg-[#121316] text-white p-6 flex flex-col items-start justify-center">
-          <h2 className="text-xl font-extrabold mb-4">My Contact Info :</h2>
+          <h2 className="text-xl mukta font-extrabold mb-4">My Contact Info :</h2>
           <p className="text-sm mb-2 sm:text-center text-left">
-            <span className="font-semibold text-md mr-4">Email :</span> meard06@gmail.com
+            <span className="font-semibold mukta text-md mr-4">Email :</span> meard06@gmail.com
           </p>
           <p className="text-sm sm:text-center text-left">
-            <span className="font-semibold text-md mr-3">Phone :</span> +91 8695260699
+            <span className="font-semibold mukta text-md mr-3">Phone :</span> +91 8695260699
           </p>
         </div>
 
 
         <div className="w-full md:w-2/3 p-6">
-          <h2 className="text-xl font-extrabold mukta mb-4 text-gray-800">Get in Touch :</h2>
+          <h2 className="text-3xl font-extrabold mukta mb-4 text-gray-800">Get in Touch :</h2>
           <form className="space-y-4" action="https://api.web3forms.com/submit" method="POST">
           <input type="hidden" name="access_key" value="041feffe-0e52-4a22-858e-514df26fcbba"/>
             <div>
-              <label className="block text-sm font-semibold mb-2 text-gray-700" htmlFor="name">Name</label>
+              <label className="block mukta text-sm font-semibold mb-2 text-gray-700" htmlFor="name">Name</label>
               <input
                 id="name"
                 type="name"
                 name="name"
                 required
-                className="w-full px-4 py-2 text-sm border border-gray-300 rounded-lg focus:outline-none"
+                className="w-full px-4 py-2 sha mukta text-sm border border-gray-300 rounded-lg focus:outline-none"
                 placeholder="Your Name"
               />
             </div>
             <div>
-              <label className="block text-sm font-semibold mb-2 text-gray-700" htmlFor="email">Email</label>
+              <label className="block mukta text-sm font-semibold mb-2 text-gray-700" htmlFor="email">Email</label>
               <input
                 id="email"
                 type="email"
                 name="email"
                 required
-                className="w-full px-4 py-2 text-sm border border-gray-300 rounded-lg focus:outline-none"
-                placeholder="your-email@example.com"
+                className="w-full px-4 mukta sha py-2 text-sm border border-gray-300 rounded-lg focus:outline-none"
+                placeholder="youremail@gmail.com"
               />
             </div>
             <div>
-              <label className="block text-sm font-semibold mb-2 text-gray-700" htmlFor="message">Message</label>
+              <label className="block text-sm mukta font-semibold mb-2 text-gray-700" htmlFor="message">Message</label>
               <textarea
                 id="message"
                 name="message"
                 required
-                className="w-full px-4 py-2 text-sm border border-gray-300 rounded-lg focus:outline-none"
+                className="w-full px-4 sha mukta py-2 text-sm border border-gray-300 rounded-lg focus:outline-none"
                 rows={4}
                 placeholder="Your message..."
               ></textarea>
             </div>
             <button
               type="submit"
-              className="w-full bg-gradient-to-bl from-[#D29EFB] to-[#590797] text-white font-semibold px-4 py-2 transition-all divide-neutral-300 hover:scale-95 rounded-lg focus:outline-none"
+              className="w-full mukta bg-gradient-to-bl from-[#D29EFB] to-[#590797] text-white font-semibold px-4 py-2 transition-all divide-neutral-300 hover:scale-95 rounded-lg focus:outline-none"
             >
               Send Message
             </button>
