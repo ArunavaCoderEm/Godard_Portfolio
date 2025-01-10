b71732f0c4e27419bb55fae0b79d3bea33805c59
ArunavaCoderEm <acecankill@gmail.com>
2025-01-11 00:16:05 +0530

    lot for today after OOP

--- src/Pages/ProductPage.tsx
+++ src/Pages/ProductPage.tsx
@@ -1,21 +1,30 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { projectsList, ProjLbImg } from "../Context/Project";
 
 export default function ProductPage(): React.ReactNode {
 
-  const param = useParams();
+    
+    const param = useParams();
+    
+    const id: number | undefined = Number(param.id);
 
-  const id: number | undefined = Number(param.id);
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        })
+    }, [param.id]);
 
   const element: ProjLbImg = projectsList[id ?? 0];
 
 
   return (
     <motion.div
-      initial={{ opacity: 0.5, y: 100 }}
-      whileInView={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0.5 }}
+      whileInView={{ opacity: 1 }}
       transition={{
         delay: 0.0,
         duration: 0.4,
@@ -23,7 +32,7 @@ export default function ProductPage(): React.ReactNode {
       }}
       className="grid grid-cols-1 gap-y-10 mt-20 mukta relative h-auto"
     >
-      ProductPage
+      <h1 className="text-white">{element?.label}</h1>
     </motion.div>
   );
 }
