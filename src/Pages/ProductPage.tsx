7b16530a90cf99dfd1248f5e8a5a340ffa9b0f06
ArunavaCoderEm <acecankill@gmail.com>
2025-01-12 00:02:56 +0530

    check

--- src/Pages/ProductPage.tsx
+++ src/Pages/ProductPage.tsx
@@ -27,22 +27,22 @@ export default function ProductPage(): React.ReactNode {
         duration: 0.4,
         ease: "easeInOut",
       }}
-      className="grid m-5 z-[103] rounded-md min-h-screen bg-[#070707] grid-cols-1 gap-y-10 mt-20 mukta relative h-auto"
+      className="grid m-5 z-[103] rounded-md min-h-screen bg-[#070707] grid-cols-1 gap-y-10 mt-20 p-1 md:p-3 mukta relative h-auto"
     >
       <div className="flex flex-col h-screen">
 
-        <div className="bg-red-500 flex-shrink-0 flex-grow-0 h-[15%] w-full">
+        <div className="bg-red-500 rounded-md flex-shrink-0 flex-grow-0 h-[15%] w-full">
           ui
         </div>
 
 
-        <div className="bg-green-500 flex-grow h-[70%] w-full">iou</div>
+        <div className="bg-green-500 rounded-md flex-grow h-[70%] w-full">iou</div>
 
 
-        <div className="bg-blue-500 flex-shrink-0 flex-grow-0 h-[15%] w-full">
+        <div className="bg-blue-500 rounded-md flex-shrink-0 flex-grow-0 h-[15%] w-full">
           end
         </div>
-        
+
       </div>
     </motion.div>
   );
